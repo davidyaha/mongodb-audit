58358808715fea45108e299acee3ec82c55f91ba
David Yahalomi <davidyaha@gmail.com>
2017-06-08 14:41:32 +0300

    Fix collectionJournaler

--- src/journalers/collection-journaler.js
+++ src/journalers/collection-journaler.js
@@ -4,7 +4,8 @@ import type { Collection } from 'mongodb';
 
 export const collectionJournaler = (journalingCollection: Collection) =>
   (collectionName: string) =>
-    (record: Object) => journalingCollection.insertOne({ collectionName, record })
+    (record: Object) => Promise.resolve()
+      .then(() => journalingCollection.insertOne({ collectionName, record: JSON.stringify(record) }))
       .then(() => true)
       .catch(e => {
         console.warn(`Could not insert to journaling collection ${collectionName} due to error`, e);
